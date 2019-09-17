b96deda9cffc35f9cec25de72e8f4cc59e881c23
Armando Flores <flor3170@otterstat.org>
2019-09-17 00:12:28 -0700

    STAT210-HW4 Files

---
+++ HW4_Pt2.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay)█, !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
+getwd()
+#> [1] "/Users/hadley/Documents/r4ds/r4ds"
+
+library(tidyverse)
+
+ggplot(diamonds, aes(carat, price)) + 
+  geom_hex()
+ggsave("diamonds.pdf")
+
+write_csv(diamonds, "diamonds.csv")
